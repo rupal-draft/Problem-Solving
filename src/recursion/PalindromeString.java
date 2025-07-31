7333133a9513bd99b06ded4ce51b0eb3b1df7d8b
Rupal Paul <rupalpaulgspr@gmail.com>
2025-07-31 12:29:16 +0530

    Palindromic string

---
+++ src/recursion/PalindromeString.java
@@ -0,0 +1,25 @@
+package recursion;
+
+public class PalindromeString {
+
+    public static void main(String[] args) {
+        String s = "racecar";
+        if (isPalindrome(s, 0, s.length()-1)){
+            System.out.println(s + " is a palindrome");
+        } else {
+            System.out.println(s + " is not a palindrome");
+        }
+    }
+
+    static boolean isPalindrome(String s, int start, int end){
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return isPalindrome(s, start+1, end-1);
+    }
+}
